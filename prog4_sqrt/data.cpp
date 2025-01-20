d488e287d210092ced274271ae7e616a211e0142
Anindit Gopalakrishnan <aninditg98@gmail.com>
2025-01-20 02:16:09 +0000

    attempts, didnt finish saxpyStreaming

--- prog4_sqrt/data.cpp
+++ prog4_sqrt/data.cpp
@@ -14,7 +14,7 @@ void initGood(float *values, int N) {
     for (int i=0; i<N; i++)
     {
         // Todo: Choose values
-        values[i] = 1.0f;
+        values[i] = 2.998f;
     }
 }
 
@@ -22,8 +22,14 @@ void initGood(float *values, int N) {
 void initBad(float *values, int N) {
     for (int i=0; i<N; i++)
     {
-        // Todo: Choose values
-        values[i] = 1.0f;
+        if (i % 8 != 0)
+        {
+            values[i] = 1.0f;
+        }
+        else
+        {
+            values[i] = 2.998f;
+        }
     }
 }
 
--- prog5_saxpy/saxpyStreaming.cpp
+++ prog5_saxpy/saxpyStreaming.cpp
@@ -16,7 +16,18 @@ void saxpyStreaming(int N,
                     float Y[],
                     float result[])
 {
-    // Replace this code with ones that make use of the streaming instructions
-    saxpySerial(N, scale, X, Y, result);
+    __m128 scale_vec = _mm_set1_ps(scale);
+
+    for (int i = 0; i < N; i += 1) {
+        __m128i result_int_x = _mm_stream_load_si128((__m128i*)X + i);
+        __m128 result_float_x = _mm_castsi128_ps(result_int_x);
+
+        __m128i result_int_y = _mm_stream_load_si128((__m128i*)Y + i);
+        __m128 result_float_y = _mm_castsi128_ps(result_int_y);
+
+        __m128 result_float = _mm_add_ps(_mm_mul_ps(scale_vec, result_float_x), result_float_y);
+        _mm_stream_ps(result + i, result_float);
+
+    }
 }
 
